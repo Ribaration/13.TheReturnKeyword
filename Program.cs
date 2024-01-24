afeeeec2b9d51475671ea6935a18b1ec081f41b0
Dell-User <Dell-User@DESKTOP-221INCO>
2024-01-24 07:58:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.TheReturnKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling methods with a return type
+            Console.WriteLine(cube(5));
+            //this wont give anything to console
+            cube(5);
+
+            //freezing the console
+            Console.ReadLine();
+        }
+        //Declare a method with a return type
+        static int cube(int number)
+        {
+            int results = number * number * number;
+            return results;
+        }
+    }
+}
